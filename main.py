a7a23f15d2b274061f56592b40516d563194f2d9
mifoxti <microfoxti@gmail.com>
2024-03-21 20:36:47 +0300

    Добавлены файлы установки, обновлена README.md и другие изменения

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+# main.py
+
 import asyncio
 import logging
 import sys
-from os import getenv
-from bot import dp, bot, main  # Импорт необходимых модулей из файла bot
+from bot import dp, bot, main
 
 if __name__ == "__main__":
-    # Настройка системы логирования для вывода информационных сообщений в стандартный поток вывода
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
-    # Запуск главной функции программы в асинхронном режиме
     asyncio.run(main())
